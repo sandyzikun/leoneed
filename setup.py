001b7c9b48843d112f4b9ca3d44cc520e422e726
Your Name <you@example.com>
2022-04-25 12:21:55 +0000

    nothing2say, again

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env Python
+# -*- coding: utf-8 -*-
+import setuptools
+setuptools.setup(
+    name = "leoneed",
+    version = "0.0.1",
+    description = "A Simple Trial on Tensor-Graph-based Network...",
+    long_description = open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/sandyzikun/leoneed.git",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ],
+    packages = setuptools.find_packages(),
+    install_requires = ["numpy>=1.14.3", "matplotlib>=2.2.2"],
+    )
